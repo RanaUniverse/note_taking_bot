9809551a9542cca55005ff2d7446173e97abc737
RanaUniverse <RanaUniverse321@gmail.com>
2025-03-20 16:09:19 +0530

    added a email validation class logic in a module

---
+++ my_modules/pydantic_validation_logic.py
@@ -0,0 +1,39 @@
+"""
+This module will have some validataion logic i will make and use in my projects
+"""
+
+from pydantic import BaseModel, Field, EmailStr, ValidationError
+
+from faker import Faker
+
+fake = Faker()
+
+
+class UserEmailValidate(BaseModel):
+    """
+    This Email is just my own wish of what i want
+    """
+
+    email: EmailStr = Field(min_length=10)
+
+
+def main():
+    """Just for checking"""
+    user_email_from_input = "a@d.com"
+
+    try:
+        email_instance = UserEmailValidate(email=user_email_from_input)
+        output_email = email_instance.email
+        print("Valid Email:", output_email)
+
+    except ValidationError as e:
+        fake_email = fake.email()
+        print("Validation Error:", e)
+        print("So, a new email is being assigned for you.")
+        email_instance = UserEmailValidate(email=fake_email)
+        print(f"Your new email is: {email_instance.email}")
+
+
+if __name__ == "__main__":
+    print("Somehign")
+    main()
