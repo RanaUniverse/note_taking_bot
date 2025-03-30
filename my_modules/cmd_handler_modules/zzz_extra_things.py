c49960a8c62c9b4f001e5bdd3cbb9387f0c80a05
RanaUniverse <RanaUniverse321@gmail.com>
2025-03-30 23:10:39 +0530

    some extra logic checked in the /rana

--- my_modules/cmd_handler_modules/zzz_extra_things.py
+++ my_modules/cmd_handler_modules/zzz_extra_things.py
@@ -7,7 +7,7 @@ from telegram.constants import ParseMode
 from telegram.ext import ContextTypes
 
 
-async def rana_checking(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+async def rana_checking_old(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """This is for checking purpose only"""
 
     if update.message is None or update.message.from_user is None:
@@ -25,6 +25,34 @@ async def rana_checking(update: Update, context: ContextTypes.DEFAULT_TYPE) -> N
         reply_markup=ReplyKeyboardMarkup(
             yes_no_reply_keyboard,
             one_time_keyboard=True,
-            input_field_placeholder="yes not button here."
+            input_field_placeholder="yes not button here.",
         ),
     )
+
+
+async def rana_checking_1(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """This is for checking purpose only"""
+
+    if update.message is None or update.message.from_user is None:
+        print("I used this to prevent the type hint of pyright. in ranachecking")
+        return
+
+    user = update.message.from_user
+    text = f"Please send me a text now,"
+
+    await context.bot.send_message(
+        chat_id=user.id,
+        text=text,
+    )
+
+
+async def rana_checking(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """This is for checking purpose only"""
+
+    if update.message is None:
+        print("just to warning remove of the below pylance")
+        return
+
+    text = f"Please send me a texts"
+
+    await update.message.reply_text(text=text, do_quote=True)
