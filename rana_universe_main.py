33883b570a27f070c5a5763ae0909e7dd56a9f95
RanaUniverse <RanaUniverse321@gmail.com>
2025-06-22 15:09:28 +0530

    a basic example of how to get random emojies from the emoji list from telegram.constants

--- rana_universe_main.py
+++ rana_universe_main.py
@@ -1,32 +1,22 @@
-"""
-This will say how many functions are there
-and their line numbers.
-"""
+import random
+from telegram.constants import ReactionEmoji
 
-import ast
+random_emoji = random.choice(list(ReactionEmoji))
 
-
-def get_function_info(filename):
-    with open(filename, "r") as file:
-        tree = ast.parse(file.read(), filename=filename)
-
-    functions = []
-
-    for node in ast.walk(tree):
-        if isinstance(node, (ast.FunctionDef, ast.AsyncFunctionDef)):
-            func_type = "async def" if isinstance(node, ast.AsyncFunctionDef) else "def"
-            functions.append(
-                {"name": node.name, "type": func_type, "line": node.lineno}
-            )
-
-    return functions
+print(random_emoji.value)
 
 
-# Example usage
-file_location = "my_modules/database_code/db_functions.py"
-function_info = get_function_info(file_location)
 
-print(f"Total functions: {len(function_info)}")
-print("Functions:")
-for func in function_info:
-    print(f" - {func['type']} {func['name']} (line {func['line']})")
+
+
+# Select only the specific emojis you want
+selected_emojis = [
+    ReactionEmoji.THUMBS_UP,
+    ReactionEmoji.TROPHY,
+    ReactionEmoji.BANANA
+]
+
+# Pick one at random
+random_emoji = random.choice(selected_emojis)
+
+print(random_emoji.value)
